0cea7111a95fae32bc8b16842734991579eddbf9
Harshh-byte <rockstarharsh7jan@gmail.com>
2026-01-31 00:56:58 +0530

    v 3.2

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 import admin from "firebase-admin";
 import { readFileSync } from "fs";
 
+const serviceAccountPath = resolve(process.cwd(), 'serviceAccountKey.json');
 const serviceAccount = JSON.parse(readFileSync("./serviceAccountKey.json"));
 
 admin.initializeApp({
